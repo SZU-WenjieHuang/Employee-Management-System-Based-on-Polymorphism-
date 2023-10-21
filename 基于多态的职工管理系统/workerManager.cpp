ce8230374cff04294cb628a93c8e9b2d9eff7287
Janki <Janki@PATRICKHUANG>
2023-10-21 15:01:40 +0800

    添加项目文件。

---
+++ 基于多态的职工管理系统/workerManager.cpp
@@ -0,0 +1,33 @@
+﻿#include "workerManager.h"
+
+WorkerManager::WorkerManager()
+{
+}
+
+
+void WorkerManager::Show_Menu() //在类的外部实现函数就需要 WorkerManager:: 来表示作用域解析
+{
+	cout << "********************************************" << endl;
+	cout << "*********  欢迎使用职工管理系统！ **********" << endl;
+	cout << "*************  0.退出管理程序  *************" << endl;
+	cout << "*************  1.增加职工信息  *************" << endl;
+	cout << "*************  2.显示职工信息  *************" << endl;
+	cout << "*************  3.删除离职职工  *************" << endl;
+	cout << "*************  4.修改职工信息  *************" << endl;
+	cout << "*************  5.查找职工信息  *************" << endl;
+	cout << "*************  6.按照编号排序  *************" << endl;
+	cout << "*************  7.清空所有文档  *************" << endl;
+	cout << "********************************************" << endl;
+	cout << endl;
+}
+
+void WorkerManager::exitSystem()
+{
+	cout << "欢迎下次使用" << endl;
+	system("pause");
+	exit(0);
+}
+
+WorkerManager::~WorkerManager()
+{
+}
---
+++ 基于多态的职工管理系统/workerManager.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#include<iostream>
+using namespace std;
+
+
+class WorkerManager
+{
+public:
+
+	//构造函数
+	WorkerManager();
+
+	//展示菜单
+	void Show_Menu();
+
+	void exitSystem();
+
+	//析构函数
+	~WorkerManager();
+
+};
